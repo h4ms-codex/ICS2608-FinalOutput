dcd0e59a0c1c4c6e19cc18e76644f0fd368d27f6
NashTabios <fredricknash.tabios.cics@ust.edu.ph>
2023-12-05 17:40:23 +0800

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>About Us</title>
-    <link rel="stylesheet" href="about_us.css" />
+    <!-- <link rel="stylesheet" href="about_us.css" /> -->
   </head>
 
   <body style="background-color: C69D9D">
