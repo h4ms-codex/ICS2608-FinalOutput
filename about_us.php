e4b55de3bd5f4838b46546941e4b15a6388e1716
NashTabios <fredricknash.tabios.cics@ust.edu.ph>
2023-12-05 18:22:16 +0800

    Update

--- about_us.php
+++ about_us.php
@@ -1,24 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="about_us.css" />
-  </head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #F2E2D4;
+        }
 
-  <body style="background-color: C69D9D">
-    <nav class="navbar">
-      <ul>
-        <li><a href="menu.html">Menu</a></li>
-        <li><a href="about_us.html">About Us</a></li>
-        <li><a href="order_cart.html">Cart</a></li>
-        <li><a href="login.html">Sign In</a></li>
-      </ul>
+        nav {
+            background-color: #C69D9D;
+            overflow: hidden;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px; /* Adjust padding as needed */
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            padding: 14px 16px;
+        }
+
+        nav a:hover {
+            background-color: #ddd;
+            color: #C69D9D;
+        }
+
+        .logo {
+            width: 65px; /* Adjust the width as needed */
+            height: 50px;
+        }
+
+        .menu-about-links {
+            display: flex;
+            gap: 10px; /* Adjust the gap between "Menu" and "About Us" */
+        }
+
+        .right-links {
+            display: flex;
+            gap: 10px; /* Adjust the gap between "Cart" and "Sign In" */
+        }
+    </style>
+</head>
+<body>
+
+    <nav>
+        <div class="menu-about-links">
+            <img src="images/Liempohan_Logo.png" alt="logo" class="logo">
+            <a href="menu.html">Menu</a>
+            <a href="about_us.html">About Us</a>
+        </div>
+        <div class="right-links">
+            <a href="order_cart.html">Cart</a>
+            <a href="login.html">Sign In</a>
+        </div>
     </nav>
 
-    <center>
-      <b>About Us </b>
-    </center>
-  </body>
+    <div>
+        <center>
+            <h2>About Us</h2>
+        </center>
+    </div>
+
+</body>
 </html>
