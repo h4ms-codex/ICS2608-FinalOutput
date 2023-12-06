e31444cc213fbff4860f13f548ee01e3c695a068
ninikoru <152066514+ninikoru@users.noreply.github.com>
2023-12-06 16:53:56 +0800

    update

--- user/order_cart.php
+++ user/order_cart.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Order Cart</title>
 
     <link href="/GitHub/ICS2608-FinalOutput/css/order_cart.css" rel="stylesheet" >
 
@@ -13,10 +13,11 @@
 <body>
     <?php include '../includes/header-user.php';
     ?>
+    <br><br>
     <h1>Your Cart</h1>
 <br>
 
-<div class="container">
+<div class="ordercart-container">
 <div class="row">
 <div class="col-2"></div>
 <div class="col-6">
@@ -73,9 +74,11 @@
 </div>
 
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+
+
+
 </body>
 
 </html>
