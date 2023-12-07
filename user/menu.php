378190bd6bc696f69d472e546eeba954d27de81d
Aisiahh <aisiah.aseron.cics@ust.edu.ph>
2023-12-07 11:09:04 +0800

    menu css(not finished)

--- user/menu.php
+++ user/menu.php
@@ -4,12 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="menu.css">
+    <title>Menu</title>
 </head>
 
 <body>
-    <?php include '../includes/header-user.php';
-    ?>
+    <div class="menu-item">
+        <span class="dish">Giniling</span>
+        <span class="price">₱60</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">Chicken Curry</span>
+        <span class="price">₱60</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">Bicol Express</span>
+        <span class="price">₱60</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">Spaghetti</span>
+        <span class="price">₱60</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">Coke</span>
+        <span class="price">₱20</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">Mountain Dew</span>
+        <span class="price">₱20</span>
+    </div>
+    <div class="menu-item">
+        <span class="dish">San Miguel Beer</span>
+        <span class="price">₱30</span>
+    </div>
+
+    <button>Order Now</button>
 </body>
 
 </html>
