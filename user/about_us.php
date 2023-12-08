985c08d168d2b3fe9a3ae98c970869f750820c23
NashTabios <fredricknash.tabios.cics@ust.edu.ph>
2023-12-08 15:48:11 +0800

    sa

--- user/about_us.php
+++ user/about_us.php
@@ -13,7 +13,7 @@
     <br>
     <h1>About Us</h1>
     <div class="aboutus-container">
-        <img src="images/Restaurant.jpg" alt="">
+        <img src="images/Restaurant.jpg" alt="Restaurant">
         <div class="aboutus-text">
             <h2>Liempohan sa Antonio</h2>
             <p>is a local restaurant chain located in 1258 Antonio St, Sampaloc, Manila, 1015 Metro Manila. We specialize in selling all sorts of Filipino food such as liempo and sisig, all homemade. Our open hours is from 8AM-6PM every weekday (Monday-Friday) and is closed every weekend (Saturday & Sunday). </p>
